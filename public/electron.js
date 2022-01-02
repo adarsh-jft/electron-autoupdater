4c68ebdf16ba06f7c400286f154458369a98d24a
Adarsh Pal <adarsh.pal@jellyfishtechnologies.com>
2022-01-02 19:54:14 +0530

    auto update

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
+const autoUpdater = require('electron-updater')
 const paths = require('path')
     // const path = require('path')
 const fs = require('fs')
 const isDev = require('electron-is-dev')
+
+let updater;
 let template = [
     { label: 'File', submenu: [{ label: 'Logout' }, { label: 'Quit', role: 'quit' }] }
 ]
@@ -25,17 +28,61 @@ function createWindow() {
 }
 
 app.whenReady().then(() => {
+    autoUpdater.checkForUpdates()
     createWindow()
 }).catch(err => {
     console.log(err)
 })
 
-// ipcMain.on("copy", (event, args) => {
-//     fs.copyFile("/home/jft-f01/Pictures/lot102.png", (paths.join(__dirname, 'InProcess/', `lot102.png`)), (err) => {
-//         if (err) {
-//             console.log(err)
-//         } else {
-//             console.log("copied")
-//         }
-//     })
-// })
+autoUpdater.autoDownload = false
+
+autoUpdater.on('error', (err) => {
+    console.log("error..........")
+    dialog.showErrorBox('Error: ', err == null ? "Unknown" : (err.stack || err).toString())
+})
+
+autoUpdater.on('update-available', () => {
+    console.log('update available.............')
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Found Updates',
+        message: 'Found updates, do you want to update now ?',
+        buttons: ['Sure', 'No']
+    }).then(buttonIndex => {
+        if (buttonIndex === 0) {
+            autoUpdater.downloadUpdate()
+        } else {
+            updater.enabled = true
+            updater = null
+        }
+    })
+})
+
+autoUpdater.on('update-not-available', () => {
+    console.log('update not available..............')
+    dialog.showMessageBox({
+        title: 'No Updates',
+        message: 'Current version is up to date'
+    })
+    updater.enabled = true
+    updater = null
+})
+
+autoUpdater.on('updated-download', () => {
+        console.log('update download.................')
+        dialog.showMessageBox({
+            title: 'Install Updates',
+            message: 'Updates downloaded, application will be closed for update...'
+        }).then(() => {
+            setImmediate(() => autoUpdater.quitAndInstall())
+        })
+    })
+    // ipcMain.on("copy", (event, args) => {
+    //     fs.copyFile("/home/jft-f01/Pictures/lot102.png", (paths.join(__dirname, 'InProcess/', `lot102.png`)), (err) => {
+    //         if (err) {
+    //             console.log(err)
+    //         } else {
+    //             console.log("copied")
+    //         }
+    //     })
+    // })
