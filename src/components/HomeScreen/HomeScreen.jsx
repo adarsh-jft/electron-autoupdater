074694a83ab5a1e01d75af7779ca754573845cec
Adarsh Pal <adarsh.pal@jellyfishtechnologies.com>
2022-01-05 15:12:50 +0530

    0.6.0

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -18,7 +18,7 @@ const HomeScreen = () => {
           <p>
             No Photos are loaded for processing <br></br> To retreive images off
             of your camera/SD card, click on <br></br> the Retrieve Photos and
-            hello new version should be available now
+            hello new version should be available now............
           </p>
           <Button>COPY</Button>
           <Button onClick={checkForUpdates}>Check for updates</Button>
