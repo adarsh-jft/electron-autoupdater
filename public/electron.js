85417573794934abc006701cffe33f3800841b1b
Adarsh Pal <adarsh.pal@jellyfishtechnologies.com>
2022-01-05 15:05:45 +0530

    0.5.0

--- public/electron.js
+++ public/electron.js
@@ -77,6 +77,16 @@ autoUpdater.on('update-available', () => {
         // })
 })
 
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+});
+
 autoUpdater.on('update-not-available', () => {
     sendStatusToWindow('update not available..............')
         // dialog.showMessageBox({
